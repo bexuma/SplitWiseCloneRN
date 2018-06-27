4b8b2d93954927e1a05833df158844a89b377b17
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2018-06-27 15:53:15 +0600

    Created app, added react-navigation, created home and addFriend screens

---
+++ AddFriendScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class AddFriendScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>AddFriendScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default AddFriendScreen;
+
--- App.js
+++ App.js
@@ -1,17 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import AddFriendScreen from './AddFriendScreen';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+
+        <View style={styles.indebtedness}>
+          <View style={{flex: 1}}>
+            <Text style={styles.textCentered}>
+              You owef
+            </Text>
+            <Text style={styles.textCentered}>
+              0 ₸
+            </Text>
+
+          </View>
+          <View style={{flex: 1}}>
+            <Text style={styles.textCentered}>
+              You are owed
+            </Text>
+            <Text style={styles.textCentered}>
+              0 ₸
+            </Text>
+
+          </View>
+          <View style={{flex: 1}}>
+            <Text style={styles.textCentered}>
+              Total balance
+            </Text>
+            <Text style={styles.textCentered}>
+              0 ₸
+            </Text>
+
+          </View>
+        </View>
+
+        <View style={{height: 100}}>
+          <Text style={[styles.textCentered, {paddingTop: 40, fontSize: 18 }]}>
+            You have not added any friends yet
+          </Text>
+
+        </View>
+
+
+        <View style={{flex: 1}}>
+          <View style={styles.button}>
+            <Button
+              title="+ Add friends on splitwise"
+              onPress={() => this.props.navigation.navigate('AddFriend')}
+            />
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    AddFriend: AddFriendScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 
 const styles = StyleSheet.create({
   container: {
@@ -20,4 +77,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  indebtedness: {
+    flexDirection: 'row',
+    height: 50,
+    borderBottomColor: '#f4f4f4',
+    borderBottomWidth: 1,
+    paddingTop: 8
+  },
+  button: {
+    paddingHorizontal: 20
+  },
+  textCentered: {
+    textAlign: 'center' 
+  }
 });
+
+
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
