c0cff04b914eb480e9421c068c092607093a8d80
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2018-07-02 18:56:39 +0600

    Gold

--- HomeScreen.js
+++ HomeScreen.js
@@ -28,41 +28,16 @@ class HomeScreen extends React.Component {
     const eachShouldPay = totalAmount / (involvedFriendsNames.length + 1)
     let transactions = []
 
-    if (paidFriendName === "You") {
-      for(let i = 0; i < involvedFriendsNames.length; i++) {
-        transactions = [{description: description, name: involvedFriendsNames[i], balance: eachShouldPay}, ...transactions]
-      }   
-      
-    } else {
-      for(let i = 0; i < involvedFriendsNames.length; i++) {
-        transactions = [{description: description, name: involvedFriendsNames[i], balance: -eachShouldPay}, ...transactions]
-      }
+    for(let i = 0; i < involvedFriendsNames.length; i++) {
+      transactions = (paidFriendName === "You")
+                ? [{description: description, name: involvedFriendsNames[i], balance: eachShouldPay}, ...transactions]
+                : [{description: description, name: involvedFriendsNames[i], balance: -eachShouldPay}, ...transactions]
     }
 
     this.setState({
       transactions: [...transactions, ...this.state.transactions]
     })
     
-  }
-
-  iOwe = () => {
-    let iOwe = 0
-    for(let i = 0; i < this.state.friends.length; i++) {
-      if (TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions) < 0) {
-        iOwe += TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions)
-      }
-    }
-    return -iOwe
-  }
-
-  iAmOwed = () => {
-    let iAmOwed = 0
-    for(let i = 0; i < this.state.friends.length; i++) {
-      if (TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions) > 0) {
-        iAmOwed += TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions)
-      }
-    }
-    return iAmOwed
   }
 
   render() {
@@ -79,27 +54,21 @@ class HomeScreen extends React.Component {
             <Text style={styles.textCentered}>
               You owe
             </Text>
-            <Text style={this.oweStyle()}>
-              {this.iOwe()} ₸
-            </Text>
+            {this.oweView()}
 
           </View>
           <View style={{flex: 1}}>
             <Text style={styles.textCentered}>
               You are owed
             </Text>
-            <Text style={this.owedStyle()}>
-              {this.iAmOwed()} ₸
-            </Text>
+            {this.owedView()}
 
           </View>
           <View style={{flex: 1}}>
             <Text style={styles.textCentered}>
               Total balance
             </Text>
-            <Text style={this.balanceStyle()}>
-              {this.iAmOwed() - this.iOwe()} ₸
-            </Text>
+            {this.balanceView()}
 
           </View>
         </View>
@@ -129,22 +98,56 @@ class HomeScreen extends React.Component {
         />
 
       </View>
-    );
+    )
   }
 
-  static navigationOptions = {
-    title: 'Splitwise',
-    headerStyle: {
-      backgroundColor: '#159688',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    }
-  };
+  oweView = () => {
+    return (
+      <Text style={this.oweStyle(this.owe())}>
+        {this.owe()} ₸
+      </Text>
+    )
+  }
 
-  oweStyle = () => {
-    return (this.state.owe !== 0)
+  owedView = () => {
+    return (
+      <Text style={this.owedStyle(this.owed())}>
+        {this.owed()} ₸
+      </Text>
+    )
+  }
+
+  balanceView = () => {
+    return (
+      <Text style={this.balanceStyle(this.owed() - this.owe())}>
+        {this.owed() - this.owe()} ₸
+      </Text>
+    )
+  }
+
+  owe = () => {
+    let owe = 0
+    for(let i = 0; i < this.state.friends.length; i++) {
+      if (TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions) < 0) {
+        owe += TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions)
+      }
+    }
+
+    return (owe === 0) ? "0" : -owe
+  }
+
+  owed = () => {
+    let owed = 0
+    for(let i = 0; i < this.state.friends.length; i++) {
+      if (TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions) > 0) {
+        owed += TotalBalanceHelper.getAggregateBalance(this.state.friends[i].name, this.state.transactions)
+      }
+    }
+    return owed
+  }
+
+  oweStyle = (owe) => {
+    return (owe !== 0)
     ? {
       textAlign: 'center',
       color: "red",
@@ -154,8 +157,8 @@ class HomeScreen extends React.Component {
     }
   }
 
-  owedStyle = () => {
-    return (this.state.owed !== 0)
+  owedStyle = (owed) => {
+    return (owed !== 0)
     ? {
       textAlign: 'center',
       color: "green",
@@ -165,13 +168,13 @@ class HomeScreen extends React.Component {
     }
   }
 
-  balanceStyle = () => {
-    if (this.state.balance < 0) {
+  balanceStyle = (balance) => {
+    if (balance < 0) {
       return {
         textAlign: 'center',
         color: "red",
       }
-    } else if (this.state.balance > 0) {
+    } else if (balance > 0) {
       return {
         textAlign: 'center',
         color: "green",
@@ -183,6 +186,18 @@ class HomeScreen extends React.Component {
     }
   
   }
+
+  static navigationOptions = {
+    title: 'Splitwise',
+    headerStyle: {
+      backgroundColor: '#159688',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    }
+  }
+
 }
 
 const styles = StyleSheet.create({
